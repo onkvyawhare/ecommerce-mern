dd87c90f88e148ec01530ff69fefaac3b5e076fd
onkvyawhare <onkvyawhare@gmail.com>
2024-11-05 13:07:56 +0530

    final commit4

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,6 @@
 // export const BASE_URL =
 // process.env.NODE_ENV === 'development' ? 'https://ecommerce-mern-599y.onrender.com' : '';
-export const BASE_URL = '';
+export const BASE_URL = 'https://ecommerce-mern-599y.onrender.com';
 export const PRODUCTS_URL = '/api/v1/products';
 export const UPLOAD_URL = '/api/v1/upload';
 export const USERS_URL = '/api/v1/users';
